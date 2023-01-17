73dfa942c27d9eb7935477efab440725b8f7df96
stelko <stilyanmtoskov@gmail.com>
2023-01-17 23:51:59 +0000

    env file setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import axios from "axios";
-import apiKey from "./config";
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 
 // App Components
-import Nav from "./components/Nav";
-import PhotoContainer from "./components/PhotoContainer";
-import SearchForm from "./components/SearchForm";
-import RouteNotFound from "./components/RouteNotFound";
-
+import Nav from './components/Nav';
+import PhotoContainer from './components/PhotoContainer';
+import SearchForm from './components/SearchForm';
+import RouteNotFound from './components/RouteNotFound';
 const App = () => {
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -16,35 +14,84 @@ const App = () => {
   const performSearch = (topic) => {
     setLoading(true);
     axios({
-      url: `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${topic}&per_page=24&format=json&nojsoncallback=1`,
-      method: "get",
-      timeout: 8000
+      url: `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.REACT_APP_APIKEY}&tags=${topic}&per_page=24&format=json&nojsoncallback=1`,
+      method: 'get',
+      timeout: 8000,
     })
-      .then(res => {
-        setPhotos(
-          res.data.photos.photo
-        )
+      .then((res) => {
+        setPhotos(res.data.photos.photo);
         setLoading(false);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+  };
 
   return (
-    <div className="container">
+    <div className='container'>
       <SearchForm />
       <Nav />
       <Routes>
-        <Route path="/" element={<PhotoContainer getPhotos={performSearch} photos={photos} loading={loading} />} />
-        <Route path="cats" element={<PhotoContainer defaultTopic={"cats"} getPhotos={performSearch} photos={photos} loading={loading} />} />
-        <Route path="dogs" element={<PhotoContainer defaultTopic={"dogs"} getPhotos={performSearch} photos={photos} loading={loading} />} />
-        <Route path="birds" element={<PhotoContainer defaultTopic={"birds"} getPhotos={performSearch} photos={photos} loading={loading} />} />
-        <Route path="search/:topic" element={<PhotoContainer getPhotos={performSearch} photos={photos} loading={loading} />} />
-        <Route path="*" element={<RouteNotFound />} />
+        <Route
+          path='/'
+          element={
+            <PhotoContainer
+              getPhotos={performSearch}
+              photos={photos}
+              loading={loading}
+            />
+          }
+        />
+        <Route
+          path='cats'
+          element={
+            <PhotoContainer
+              defaultTopic={'cats'}
+              getPhotos={performSearch}
+              photos={photos}
+              loading={loading}
+            />
+          }
+        />
+        <Route
+          path='dogs'
+          element={
+            <PhotoContainer
+              defaultTopic={'dogs'}
+              getPhotos={performSearch}
+              photos={photos}
+              loading={loading}
+            />
+          }
+        />
+        <Route
+          path='birds'
+          element={
+            <PhotoContainer
+              defaultTopic={'birds'}
+              getPhotos={performSearch}
+              photos={photos}
+              loading={loading}
+            />
+          }
+        />
+        <Route
+          path='search/:topic'
+          element={
+            <PhotoContainer
+              getPhotos={performSearch}
+              photos={photos}
+              loading={loading}
+            />
+          }
+        />
+        <Route
+          path='*'
+          element={<RouteNotFound />}
+        />
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
